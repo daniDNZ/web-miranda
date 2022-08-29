3dc7a21d08896d6b6a3af91ac082025c843b903c
danisanz <daniDNZ@users.noreply.github.com>
2022-08-29 17:47:24 +0200

    List nearest locations

--- public/js/maps.js
+++ public/js/maps.js
@@ -2,7 +2,7 @@ import hotelLocations from '../mapsData/hotelLocations.json' assert { type: "jso
 
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 6,
     center: {
       lat: 40.343842,
       lng: -3.535011,
@@ -31,6 +31,101 @@ function initMap() {
     return marker;
   });
   const markerCluster = new markerClusterer.MarkerClusterer({ map, markers });
+
+  // Geolocation
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent("Your location.");
+        map.setCenter(pos);
+
+        const geoMarker = new google.maps.Marker({
+          position: pos,
+          map: map,
+          name: 'You'
+        });
+
+        // Find nearest location
+        const findNearestLocationButton = document.querySelector('#findNearestLocationButton');
+        findNearestLocationButton.addEventListener('click', () => configureMatrixService(pos, hotelLocations));
+      },
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+
 }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+
+function configureMatrixService(origin, destinations) {
+  const service = new google.maps.DistanceMatrixService();
+  service.getDistanceMatrix(
+    {
+      origins: [origin],
+      destinations,
+      travelMode: 'DRIVING'
+    },
+    listNearestLocations
+  );
+
+  function listNearestLocations(response, status) {
+    if (status === 'OK') {
+
+      const locationsUl = document.querySelector('#nearestLocationsList');
+      const nearestLocations = makeList(response);
+
+      nearestLocations.sort((a, b) => a.durationValue - b.durationValue);
+      nearestLocations.forEach(location => {
+        const li = document.createElement('li');
+        li.textContent = `${location.duration} - ${location.distance} - ${location.to}`;
+        locationsUl.appendChild(li);
+      }
+      )
+    }
+  }
+
+  function makeList(response) {
+    const origins = response.originAddresses;
+    const destinations = response.destinationAddresses;
+    const nearestLocations = [];
+
+    for (let i = 0; i < origins.length; i++) {
+      const results = response.rows[i].elements;
+      for (let j = 0; j < results.length; j++) {
+        const element = results[j];
+        const distance = element.distance.text;
+        const duration = element.duration.text;
+        const durationValue = element.duration.value;
+        const from = origins[i];
+        const to = destinations[j];
+
+        nearestLocations.push({ distance, duration, durationValue, from, to });
+      }
+    }
+
+    return nearestLocations;
+  }
+}
+
 
 window.initMap = initMap;
