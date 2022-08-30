4628f3c1b5f23bc7923c460a060c7ea572cc4b71
danisanz <daniDNZ@users.noreply.github.com>
2022-08-30 14:14:37 +0200

    Sprint 4 google maps finished

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,4 +1,5 @@
 import hotelLocations from '../mapsData/hotelLocations.json' assert { type: "json"};
+import comunidadesEspana from '../mapsData/comunidadesEspana.json' assert { type: "json"};
 
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
@@ -21,7 +22,7 @@ function initMap() {
     const marker = new google.maps.Marker({
       position: { lat: position.lat, lng: position.lng },
       name: position.name,
-      icon: icon
+      icon: icon,
     });
 
     marker.addListener("click", () => {
@@ -33,28 +34,30 @@ function initMap() {
   const markerCluster = new markerClusterer.MarkerClusterer({ map, markers });
 
   // Geolocation
+  let geoPos = {};
+  const userMarker = new google.maps.Marker({
+    position: { lat: 0, lng: 0 },
+    map: map,
+    name: 'You'
+  });
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        const pos = {
+        geoPos = {
           lat: position.coords.latitude,
           lng: position.coords.longitude,
         };
 
-        infoWindow.setPosition(pos);
-        infoWindow.setContent("Your location.");
-        map.setCenter(pos);
-
-        const geoMarker = new google.maps.Marker({
-          position: pos,
-          map: map,
-          name: 'You'
+        userMarker.addListener("click", () => {
+          infoWindow.setContent("Your location.");
+          infoWindow.open(map, userMarker);
         });
 
-        // Find nearest location
-        const findNearestLocationButton = document.querySelector('#findNearestLocationButton');
-        findNearestLocationButton.addEventListener('click', () => configureMatrixService(pos, hotelLocations));
+        map.setCenter(geoPos);
+
+        userMarker.setPosition(geoPos);
+
       },
       () => {
         handleLocationError(true, infoWindow, map.getCenter());
@@ -65,6 +68,133 @@ function initMap() {
   }
 
 
+
+  // List nearest locations
+  const geocoder = new google.maps.Geocoder();
+
+  const findNearestLocationButton = document.querySelector('#findNearestLocationButton');
+  findNearestLocationButton.addEventListener('click', () => {
+    const address = document.querySelector('#stringAddress').value;
+    let originLocation = {};
+
+    if (address.length > 0) {
+
+      geocoder.geocode({ 'address': address }, function (results, status) {
+        if (status == 'OK') {
+          originLocation = results[0].geometry.location;
+          map.setCenter(originLocation);
+          userMarker.setPosition(originLocation);
+          configureMatrixService(originLocation, hotelLocations)
+        } else {
+          console.error('Geocode was not successful for the following reason: ' + status);
+        }
+      });
+
+    } else if (navigator.geolocation) {
+      originLocation = { ...geoPos };
+      configureMatrixService(originLocation, hotelLocations)
+    }
+  });
+
+  // Select by Comunidad Autónoma
+
+  const comunidadesAutonomas = [
+    'Andalucía',
+    'Aragón',
+    'Asturias, Principado de',
+    'Balears, Illes',
+    'Canarias',
+    'Cantabria',
+    'Castilla y León',
+    'Castilla - La Mancha',
+    'Cataluña / Catalunya',
+    'Comunitat Valenciana',
+    'Extremadura',
+    'Galicia',
+    'Madrid, Comunidad de',
+    'Murcia, Región de',
+    'Navarra, Comunidad Foral de',
+    'País Vasco / Euskadi',
+    'Rioja, La',
+    'Ceuta',
+    'Melilla',
+  ];
+
+  const cASelect = document.querySelector('#comunidadAutonoma');
+  comunidadesAutonomas.forEach(ca => {
+    const option = document.createElement('option');
+    option.textContent = ca;
+    option.value = ca;
+    cASelect.appendChild(option);
+  })
+
+  cASelect.addEventListener('change', (e) => selectCA(e.target.value));
+
+  function selectCA(ca) {
+    const cAIndex = comunidadesAutonomas.indexOf(ca);
+    const polygonCoords = [];
+
+    if (cAIndex >= 0) {
+
+      comunidadesEspana[cAIndex].forEach(subArr =>
+        polygonCoords.push(subArr)
+      );
+
+      // Construct the polygon.
+      const polygon = new google.maps.Polygon({
+        paths: polygonCoords,
+        strokeColor: "#FF0000",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#FF0000",
+        fillOpacity: 0.35,
+      });
+
+      polygon.setMap(map);
+
+      cASelect.addEventListener('change', () => polygon.setMap(null));
+
+      // Markers 
+      markerCluster.clearMarkers();
+      const markers = [];
+      hotelLocations.forEach((position) => {
+        if (position.com === cAIndex) {
+          const marker = new google.maps.Marker({
+            position: { lat: position.lat, lng: position.lng },
+            name: position.name,
+            icon: icon,
+          });
+
+          marker.addListener("click", () => {
+            infoWindow.setContent(marker.name);
+            infoWindow.open(map, marker);
+          });
+          markers.push(marker);
+        }
+      });
+      markerCluster.addMarkers(markers);
+
+    } else {
+
+      markerCluster.clearMarkers();
+      const markers = [];
+      hotelLocations.forEach((position) => {
+        const marker = new google.maps.Marker({
+          position: { lat: position.lat, lng: position.lng },
+          name: position.name,
+          icon: icon,
+        });
+
+        marker.addListener("click", () => {
+          infoWindow.setContent(marker.name);
+          infoWindow.open(map, marker);
+        });
+        markers.push(marker);
+      });
+      markerCluster.addMarkers(markers);
+    }
+
+  }
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -92,6 +222,7 @@ function configureMatrixService(origin, destinations) {
     if (status === 'OK') {
 
       const locationsUl = document.querySelector('#nearestLocationsList');
+      locationsUl.innerHTML = '';
       const nearestLocations = makeList(response);
 
       nearestLocations.sort((a, b) => a.durationValue - b.durationValue);
@@ -111,6 +242,7 @@ function configureMatrixService(origin, destinations) {
 
     for (let i = 0; i < origins.length; i++) {
       const results = response.rows[i].elements;
+
       for (let j = 0; j < results.length; j++) {
         const element = results[j];
         const distance = element.distance.text;
